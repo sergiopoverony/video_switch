7aebfa3d02045a01ecf957940c3be2396bc7b5dc
SergioPoverony <sergiopoverony@gmail.com>
2019-06-21 00:26:18 +0500

    Fix install

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ echo "Switcher video out AV and HDMI with GPIO21 button..."
 
 #boot script should have the necessary lines(Battery, poweroff, backlight, buttons), then new ones should be added after a y/n question(joystick)
 # Insert battery script into rc.local before final 'exit 0'
-if ! grep -Fxq "python /home/pi/video_switch/test_gpio.py &" /etc/rc.local
+if ! grep -Fxq "python /home/pi/video_switch/video_switcher.py &" /etc/rc.local
 then
-sed -i "s/^exit 0/python /home/pi/video_switch/test_gpio.py \&\\nexit 0/g" /etc/rc.local >/dev/null
+sed -i "s/^exit 0/python \/home\/pi\/video_switch\/video_switcher.py \&\\nexit 0/g" /etc/rc.local >/dev/null
 fi
 
 read -rsp $'Press any key to reboot...\n' -n1 key
